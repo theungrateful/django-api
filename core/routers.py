cf1eaf6b8a84ed6d3c9616dd19c990724110c65a
theungrateful <nelson8111@gmail.com>
2023-04-07 17:48:38 -0300

    Add jwt login

--- core/routers.py
+++ core/routers.py
@@ -1,11 +1,13 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
 from core.auth.viewsets.register import RegisterViewSet
+from core.auth.viewsets.login import LoginViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'user', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
 
 urlpatterns = [
     *router.urls,

274776545575c1cf69a3fd6284d0faf602d4efc9
theungrateful <nelson8111@gmail.com>
2023-04-07 18:17:43 -0300

    Add jwt refresh

--- core/routers.py
+++ core/routers.py
@@ -1,13 +1,15 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
-from core.auth.viewsets.register import RegisterViewSet
+from core.auth.viewsets import RegisterViewSet
 from core.auth.viewsets.login import LoginViewSet
+from core.auth.viewsets import RefreshViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'user', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
 urlpatterns = [
     *router.urls,

74bf6fd962ff9c3f343dbbb5e1233636246f9ecd
theungrateful <nelson8111@gmail.com>
2023-05-23 21:02:31 -0300

    Add test for Post, Comment models and fixtures post and user

--- core/comment/tests.py
+++ core/comment/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from core.fixtures.post import post
+from core.fixtures.user import user
+from core.comment.models import Comment
+
+
+@pytest.mark.django_db
+def test_create_comment(user, post):
+    comment = Comment.objects.create(
+        author=user,
+        body="Test Comment body",
+        post=post
+    )
+    assert comment.author == user
+    assert comment.body == "Test Comment body"
+    assert comment.post == post

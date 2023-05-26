f67cb93583561dce776863ad1765d2a5a51ef3e8
theungrateful <nelson8111@gmail.com>
2023-05-26 19:36:41 -0300

    Add anonymus user tests for post

--- core/post/tests/test_viewsets.py
+++ core/post/tests/test_viewsets.py
@@ -47,4 +47,34 @@ class TestPostViewSet:
         response = client.delete(self.endpoint + str(post.public_id) + '/')
         assert response.status_code == status.HTTP_204_NO_CONTENT
 
+    def test_list_anonymous_users(self, client, post):
+        response = client.get(self.endpoint)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 1
         
+    def test_retrieve_anonymous_users(self, client, post):
+        response = client.get(self.endpoint + str(post.public_id) + '/')
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['id'] == post.public_id.hex
+        assert response.data['body'] == post.body
+        assert response.data['author']['id'] == post.author.public_id.hex
+        
+    def test_create_anonymous(self, client):
+        data = {
+            "body": "Test Post Body",
+            "author": "test_user",
+        }
+        response = client.post(self.endpoint, data)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+    def test_update_anonymous(self, client, post):
+        data = {
+            "body": "Test Post Body",
+            "author": "test_user",
+        }
+        response = client.put(self.endpoint + str(post.public_id) + '/', data)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        
+    def test_delete_anonymous(self, client, post):
+        response = client.delete(self.endpoint + str(post.public_id) + '/')
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED

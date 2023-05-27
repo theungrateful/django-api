5ea4986bff527f3b25482626438ea7885dee1bdb
theungrateful <nelson8111@gmail.com>
2023-05-27 18:53:22 -0300

    Add login page

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -4,6 +4,7 @@ import {
   Routes,
 } from "react-router-dom";
 import ProtectedRoute from "./routes/ProtectedRoute";
+import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import Home from './pages/Home';
 
@@ -16,7 +17,7 @@ function App() {
           <Home />
         </ProtectedRoute>
       } />
-      <Route path='/login/' element={<div>Login</div>} />
+      <Route path='/login/' element={<Login />} />
       <Route path='/register/' element={<Registration />} />
     </Routes>
   );
